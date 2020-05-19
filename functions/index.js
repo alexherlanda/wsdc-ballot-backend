36e20e6f06c18d7d9b68a57434c0a36f943b5b99
Alexis Herlanda <alexisherlanda@gmail.com>
2020-05-18 21:43:21 -0500

    basic functionality added. sending mails and working

--- functions/index.js
+++ functions/index.js
@@ -1,35 +1,38 @@
-const functions = require('firebase-functions');
-const express = require('express');
-const nodemailer = require('nodemailer');
 require('dotenv').config();
+const functions = require('firebase-functions');
+const nodemailer = require('nodemailer');
+const express = require('express');
+const cors = require('cors');
 const app = express();
-app.get('/home', (re, res) => {
-	//Step 1
-	let transporter = nodemailer.createTransport({
+
+app.use(cors({ origin: true }));
+
+app.post('/', (req, res) => {
+	const { body } = req;
+	const isValid = true;
+	if (!isValid) {
+		return res.status(400).send({ message: 'Algo salio mal' });
+	}
+
+	const transporter = nodemailer.createTransport({
 		service: 'gmail',
 		auth: {
-			user: process.env.PASSWORD,
-			pass: process.env.EMAIL,
+			user: process.env.EMAIL,
+			pass: process.env.PASSWORD,
 		},
 	});
 
-	// Setep 2
-	let mailOptions = {
-		from: 'wudcballot@gmail.com',
-		to: 'alexisherlada@gmail.com',
-		subject: 'México Debate WSDC Result',
-		text: 'Hola desde wsdc backend',
+	const mailOptions = {
+		from: process.env.EMAIL,
+		to: 'alexisherlanda@gmail.com',
+		subject: 'Test',
+		text: 'Hola estos son los resultados de tu debate',
 	};
 
-	//Step 3
 	transporter.sendMail(mailOptions, (err, data) => {
-		if (err) {
-			console.log(err);
-		} else {
-			console.log('Success');
-		}
+		if (err) return res.status(500).send({ message: err.message });
+		return res.send({ message: 'Email sent', data: data });
 	});
-	res.send('hello from express app backend');
 });
 
-exports.app = functions.https.onRequest(app);
+module.exports.mailer = functions.https.onRequest(app);
