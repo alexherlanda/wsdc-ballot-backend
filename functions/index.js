7e79627becc21420ed95f20b897781882a4cdaec
Alexis Herlanda <alexisherlanda@gmail.com>
2020-06-18 19:21:38 -0500

    simpplifies data

--- functions/index.js
+++ functions/index.js
@@ -22,94 +22,20 @@ app.post('/', (req, res) => {
 		},
 	});
 
+	const createSubject = () => {
+		return (
+			'Resultados de Debate ' +
+			req.body.judgeData.name +
+			req.body.callData.round
+		);
+	};
+	//TODO: Hide this
 	const mailOptions = {
 		from: process.env.EMAIL,
-		to: 'alexisherlanda@gmail.com',
-		subject: 'Test',
-		html: `
-			<style>
-				.demo {
-					border:1px solid #C0C0C0;
-					border-collapse:collapse;
-					padding:5px;
-				}
-				.demo th {
-					border:1px solid #C0C0C0;
-					padding:5px;
-					background:#F0F0F0;
-				}
-				.demo td {
-					border:1px solid #C0C0C0;
-					padding:5px;
-				}
-			</style>
-			<div>
-				<p> Juez: Alexis Herlanda </p>
-				<p> Afiliación: UNAM </p>
-				<p> Ganador: Oposición </p>
-				<br /><br /><br />
-				<table class="demo">
-				<caption>Proposición</caption>
-				<tr>
-					<th>Orador</th>
-					<th>Puntaje</th>
-				</tr>
-				</thead>
-				<tbody>
-				<tr>
-					<td>&nbsp;Total</td>
-					<td>&nbsp;240</td>
-				</tr>
-				<tr>
-					<td>&nbsp;1ero Prop</td>
-					<td>&nbsp;60</td>
-				</tr>
-				<tr>
-					<td>&nbsp;2do Prop</td>
-					<td>&nbsp;60</td>
-				</tr>
-				<tr>
-					<td>&nbsp;3ero Prop</td>
-					<td>&nbsp;60</td>
-				</tr>
-				<tr>
-					<td>&nbsp;Replica Prop</td>
-					<td>&nbsp;60</td>
-				</tr>
-				<tbody>
-				</table>
-			    <br/> <br /> <br />
-				<table class="demo">
-				<caption>Oposición</caption>
-				<tr>
-					<th>Orador</th>
-					<th>Puntaje</th>
-				</tr>
-				</thead>
-				<tbody>
-				<tr>
-					<td>&nbsp;Total</td>
-					<td>&nbsp;240</td>
-				</tr>
-				<tr>
-					<td>&nbsp;1ero Oppo</td>
-					<td>&nbsp;60</td>
-				</tr>
-				<tr>
-					<td>&nbsp;2do Oppo</td>
-					<td>&nbsp;60</td>
-				</tr>
-				<tr>
-					<td>&nbsp;3ero Oppo</td>
-					<td>&nbsp;60</td>
-				</tr>
-				<tr>
-					<td>&nbsp;Replica Oppo</td>
-					<td>&nbsp;60</td>
-				</tr>
-				<tbody>
-				</table>
-			</div>`,
+		to: process.env.EMAIL,
+		cc: 'alexisherlanda64gmail.com',
+		subject: createSubject(),
+		text: JSON.stringify(req.body),
 	};
 
 	transporter.sendMail(mailOptions, (err, data) => {
